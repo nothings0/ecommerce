4b625eadc97051155e4b6a881a07b25d781d1832
Nothings0 <nhanphamx3@gmail.com>
2023-08-22 14:41:25 +0700

    fix json

--- client/app/Hooks/useFetchAddress.ts
+++ client/app/Hooks/useFetchAddress.ts
@@ -6,7 +6,9 @@ export default function useFetchAddress<T = unknown>(path: string) {
   const [data, setData] = useState<T | null>(null);
   useEffect(() => {
     const getData = async () => {
-      const res = await axios.get(`http://localhost:3000${path}`);
+      const res = await axios.get(
+        `https://ecommerce-rust-beta.vercel.app${path}`
+      );
       const resData: T = res.data;
       setData(resData);
     };
