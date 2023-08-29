eece4c8e40ace404498e1f1ba45674fc7867cc90
Nothings0 <nhanphamx3@gmail.com>
2023-08-29 16:10:26 +0700

    up

--- client/components/ProductFamous/index.tsx
+++ client/components/ProductFamous/index.tsx
@@ -3,19 +3,22 @@ import { IResProductFamous } from "@/type";
 import ProductFamousItem from "./ProductFamousItem";
 import "./index.scss";
 import { axiosPrimary } from "@/config/axiosConfig";
-import useFetch from "@/app/Hooks/useFetch";
-// const getProduct = async () => {
-//   const res = await axiosPrimary.get(`/product-hots?populate=*`);
-//   return res.data;
-// };
+// import useFetch from "@/app/Hooks/useFetch";
+const getProduct = async () => {
+  const res = await axiosPrimary.get(`/product-hots?populate=*`);
+  return res.data;
+};
 
 const ProductFamous = async () => {
-  // const { data }: IResProductFamous = await getProduct();
-  const { data } = useFetch<any>(`product-hot`, `/product-hots?populate=*`);
+  const { data }: IResProductFamous = await getProduct();
+  // const { data } = useFetch<IResProductFamous>(
+  //   `product-hot`,
+  //   `/product-hots?populate=*`
+  // );
   return (
     <div className="product__famous">
       <div className="product__famous__container">
-        {data.map((item: any) => (
+        {data?.map((item) => (
           <ProductFamousItem key={item.id} data={item} />
         ))}
       </div>
