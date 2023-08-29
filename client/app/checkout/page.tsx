ba41ce1ec59d0306eb4ffcfa6f9f61ccf0d2c0d3
Nothings0 <nhanphamx3@gmail.com>
2023-08-29 11:23:32 +0700

    fix 2

--- client/app/checkout/page.tsx
+++ client/app/checkout/page.tsx
@@ -4,38 +4,47 @@ import useOrderStore from "@/zustand/orderSlice";
 import useProductStore from "@/zustand/productSlice";
 import useUserStore from "@/zustand/userSlice";
 import React, { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
 import "./index.scss";
+
 const page = () => {
   const { order, removeOrder } = useOrderStore();
   const { removeProduct } = useProductStore();
   const { user, jwt } = useUserStore();
-
+  const useSearch = useSearchParams();
+  const status = useSearch?.get("vnp_TransactionStatus");
   const handleOrder1 = async () => {
     const data = {
       customer_id: user?.id,
       products: order,
     };
-    try {
-      await axiosPrimary.post("/orders", data, {
-        headers: {
-          Authorization: `Bearer ${jwt}`,
-        },
-      });
-      removeOrder();
-      removeProduct(order);
-    } catch (error) {
-      console.log(error);
+    if (status === "00") {
+      try {
+        await axiosPrimary.post("/orders", data, {
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+        });
+        removeOrder();
+        removeProduct(order);
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
 
   useEffect(() => {
     handleOrder1();
-  }, []);
+  }, [status]);
 
   return (
     <div className="checkout main">
       <div className="checkout__container">
-        <h2>Thanh toán thành công!!!</h2>
+        {status === "00" ? (
+          <h2>Thanh toán thành công!!!</h2>
+        ) : (
+          <h2>Thanh toán thất bại!!!</h2>
+        )}
       </div>
     </div>
   );
